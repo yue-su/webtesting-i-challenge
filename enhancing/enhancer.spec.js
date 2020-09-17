b27a4bf3983e051cbe861d8c6da1e89d8721ec07
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-09-16 22:44:23 -0300

    added basic test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,72 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js")
 // test away!
+
+describe("enhancer", () => {
+  describe("repair", () => {
+    it("should set durability to 100", () => {
+      //act
+      const item = { name: "plot", durability: 30, enhancement: 10 }
+      const repairedItem = enhancer.repair(item)
+
+      expect(repairedItem).toEqual({
+        name: "plot",
+        durability: 100,
+        enhancement: 10,
+      })
+    })
+  })
+
+  describe("success", () => {
+    it("should add 1 to enhencement when the enhencement is less than 20 ", () => {
+      const item = { name: "plot", durability: 30, enhancement: 10 }
+      const succeedItem = enhancer.success(item)
+      
+      expect(succeedItem).toEqual({
+          name: "plot",
+          durability: 30,
+          enhancement: 11,
+        })
+    })
+    
+    it("should not change if the enhencement is 20", () => {
+        const item = { name: "plot", durability: 30, enhancement: 20 }
+        const succeedItem = enhancer.success(item)
+        
+        expect(succeedItem).toEqual({
+            name: "plot",
+            durability: 30,
+            enhancement: 20,
+          })
+    })
+  })
+    
+    describe("fail", () => {
+        it("should have durability - 5 when enhancement < 15", () => {
+           const item = { name: "plot", durability: 30, enhancement: 14 }
+            const failedItem = enhancer.fail(item)
+            expect(failedItem).toEqual({
+              name: "plot",
+              durability: 25,
+              enhancement: 14,
+            })
+        })
+        it("should have durability - 10 when enhancement = 15 or 16", () => {
+            const item = { name: "plot", durability: 30, enhancement: 16 }
+            const failedItem = enhancer.fail(item)
+            expect(failedItem).toEqual({
+              name: "plot",
+              durability: 20,
+              enhancement: 16,
+            })
+        })
+        it("should have durability - 10 and enhancement - 1 when enhancement > 16", () => {
+            const item = { name: "plot", durability: 30, enhancement: 18 }
+            const failedItem = enhancer.fail(item)
+           expect(failedItem).toEqual({
+             name: "plot",
+             durability: 20,
+             enhancement: 17,
+           })
+       })
+   }) 
+})
